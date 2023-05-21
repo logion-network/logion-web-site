31c3781d4aaf090e3c0e8bd1dfd32d0463e96e2a
Elie Auvray <83208268+elieauvray@users.noreply.github.com>
2023-05-21 21:18:21 +0200

    Update Content.tsx

--- src/Content.tsx
+++ src/Content.tsx
@@ -157,7 +157,7 @@ export const CONTENT: ReactNode = (<>
         paragraphs={<>
             { /* START EDIT */ }
             <p>Logion's <em>global</em> Judicial Officers network authenticates identities and confirm the legitimacy of corporations, establishing an <em>unbroken chain of custody</em> for evidence records, as mandated by legal and compliance principles. Verified Issuers, subject to rigorous due diligence by these officers, bear the <em>accountability</em> for the professional content they contribute.</p>
-            <p>This strong foundation enables Logion infrastructure to <em><strong>automatically record and bind</strong></em> vast amounts of token-related data, files and digital assets to <em><strong>millions of tokens</strong></em>. These records are shared by the <em>Logion certificate</em> - bound to each token - through a Zero Knowledge Proof system, preserving confidentiality and privacy while paving the way for the forthcoming wave of tokenization.</p>  { /* STOP EDIT */ }
+            <p>This strong foundation enables Logion infrastructure to <em>automatically record and bind</em> vast amounts of token-related data, files and digital assets to <em>millions of tokens</em>. These records are shared by the <em>Logion certificate</em> - bound to each token - through a Zero Knowledge Proof system, preserving confidentiality and privacy while paving the way for the forthcoming wave of tokenization.</p>  { /* STOP EDIT */ }
         </>}
         images={<>
             <Image
