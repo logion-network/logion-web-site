c7677e7399af1be16b2bedf288db73078e33c176
Elie Auvray <83208268+elieauvray@users.noreply.github.com>
2023-05-22 18:18:10 +0200

    Update Content.tsx

--- src/Content.tsx
+++ src/Content.tsx
@@ -447,16 +447,16 @@ export const CONTENT: ReactNode = (<>
     <OneByTwoTextImage
         title={<>
             { /* START EDIT */ }
-            <strong>We are proud to be financially supported by:</strong>
+            <strong>We are proud to be supported by:</strong>
             { /* STOP EDIT */ }
         </>}
         paragraphs={<>
             { /* START EDIT */ }
             <ul>
-                <li>Since its inception, Wallonie Enterprendre, a public sovereign fund (Belgium) has been supporting the developpment of the project</li>
-                <li>Since June 2022, logion has been selected by Outlier Ventures, the leading web3 and metaverse accelerator and investor</li>
-                <li>Primal Ventures, A global Web3 fund backing culture shifting ideas and innovations</li>
-                <li>In January 2022, logion recieved a grant from the Web3 foundation for its innovative social recovery and multisig capabilties</li>
+                <li>Since its inception, <em>Wallonie Entreprendre</em>, a public sovereign fund (Belgium) has been supporting the developpment of the project</li>
+                <li>Since June 2022, logion has been selected by <em>Outlier Ventures</em>, the leading web3 and metaverse accelerator and investor</li>
+                <li><em>Primal Capital</em>, A global Web3 fund backing culture shifting ideas and innovations</li>
+                <li>In January 2022, logion recieved a grant from <em>The Web3 foundation</em> for its innovative social recovery and multisig capabilties</li>
             </ul>
             { /* STOP EDIT */ }
         </>}
